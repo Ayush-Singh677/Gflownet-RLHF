bedc425ebd384979293312174a6952a613fb7415
Ayush-Singh677 <82378979+Ayush-Singh677@users.noreply.github.com>
2025-01-21 22:29:11 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -244,7 +244,7 @@ def generate_and_return_termination_logprob(
     skip_rewards=False,
 ):
     # generate and return the probability of terminating at every step
-    active_seqs = None
+    active_seqs = torch.ones(encoded_prompt.size(0)).bool().to(encoded_prompt.device)
     state = encoded_prompt.clone()
     # print("Encoded Prompt: ",state)
     log_pf = []
