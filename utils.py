893960b7d7b5dd4d47bea7baf4609406bc913b3b
Ayush-Singh677 <82378979+Ayush-Singh677@users.noreply.github.com>
2025-01-22 17:44:56 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -296,6 +296,8 @@ def generate_and_return_termination_logprob(
                         vocab_naughty_mask = vocab_naughty_mask[:desired_size]
                     modified_logits[:, vocab_naughty_mask] += vocab_alpha
                 prob = (modified_logits / temperature).softmax(dim=-1)
+                prob = (modified_logits / 0.01).softmax(dim=-1)  # Temperature near 0
+
                 # token_ids = torch.multinomial(prob, num_samples=1)
                 token_ids = torch.argmax(prob, dim=-1, keepdim=True)
 
