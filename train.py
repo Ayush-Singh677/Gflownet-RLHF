38632018573e1a7c8fc74069c6a8ca6f17189b9e
Ayush-Singh677 <82378979+Ayush-Singh677@users.noreply.github.com>
2025-01-24 19:30:18 +0530

    Update train.py

--- train.py
+++ train.py
@@ -136,7 +136,7 @@ def get_model(config: DictConfig):
     )
     model = model.to("cuda:0")
     if torch.cuda.device_count() > 1:
-        model = torch.nn.parallel.DataParallel(model)
+        model = torch.nn.parallel.DistributedDataParallel(model)
 
     if config.task.training.use_4bit:
         model = prepare_model_for_kbit_training(
