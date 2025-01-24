96a93dd263c6ecd6e3250b84212d3cb7facc2635
Ayush-Singh677 <82378979+Ayush-Singh677@users.noreply.github.com>
2025-01-24 15:51:16 +0530

    Update train.py

--- train.py
+++ train.py
@@ -135,7 +135,7 @@ def get_model(config: DictConfig):
         config.task.model.name, quantization_config=bnb_config
     )
     if torch.cuda.device_count() > 1:
-        model = torch.nn.DataParallel(model)
+        model = torch.nn.DistributedDataParallel(model)
 
     if config.task.training.use_4bit:
         model = prepare_model_for_kbit_training(
